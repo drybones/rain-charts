5c525a6d81a8a32a90da3e705efc21fdd69e61b9
David Bick <david@drybones.co.uk>
2024-12-30 16:30:55 +0000

    Initial Commit

---
+++ RainCharts/RainChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainChartsApp.swift
+//  RainCharts
+//
+//  Created by David Bick on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct RainChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
